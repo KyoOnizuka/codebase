57eda4599078fc85ea67654da07b13ae3f99f031
baothieu <trongbao0909@gmail.com>
2024-02-08 16:42:43 +0700

    update generic dao template

--- src/main/java/com/example/codebase/dao/AbstractDao.java
+++ src/main/java/com/example/codebase/dao/AbstractDao.java
@@ -1,5 +1,20 @@
 package com.example.codebase.dao;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public abstract class AbstractDao<T> {
+    protected Class<T> entityClass;
+    EntityManager entityManager;
+    public Optional<T> findByID(String Id){
+        return Optional.ofNullable(this.entityManager.find(this.entityClass, Id));
+    }
+    public Optional<List<T>> findAll() {
+        return Optional.ofNullable(this.entityManager.createQuery("SELECT a FROM " + this.entityClass.getSimpleName() + " a ").getResultList());
+    }
 
 }
---
+++ src/main/java/com/example/codebase/utils/StringBuilder.java
@@ -0,0 +1,6 @@
+package com.example.codebase.utils;
+
+public class StringBuilder {
+    private byte[] buffer;
+    private int size;
+}
